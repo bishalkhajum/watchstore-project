2f90ac8fe0d1fa9b09dac03e94b7755d7f4b2e9f
20all <bishallimbu00010@gmail.com>
2026-08-29 17:20:53 +0545

    Add VITE_API_URL to client.js

--- ecommerce-frontend/src/api/client.js
+++ ecommerce-frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 // Attach the JWT to every request if we have one.
